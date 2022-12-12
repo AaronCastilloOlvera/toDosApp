d5aa98789825ecf0d22ef77ef6170fce007798cf
AaronCastilloOlvera <aaroncastilloolvera@gmail.com>
2022-12-11 19:54:29 -0600

    Add all files created.

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// My First Component!
+function AppComponent(){
+  return (
+    <h1 id='title'>
+      Oli React
+    </h1>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>, //Component because Start with a Upper 
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
